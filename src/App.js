eef1a1de9d52de40eef2e41a40b3f17e6626b368
jdhilts <jameshilts@icloud.com>
2020-06-10 17:00:04 -0700

    Adding signin and register to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Fragment} from 'react';
 
 function App() {
   return (
+  	<Fragment>
     <h1>Hello World!</h1>
+    <Signin/>
+    </Fragment>
   );
 }
-
 export default App;
